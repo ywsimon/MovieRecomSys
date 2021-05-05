9f4e0c834cc608bef48a6b7c6d41b5c2c6a0f0fd
yrc <419107280@qq.com>
2021-05-06 02:10:51 +0800

    --story=1007765 --user=SC 后端开发 {#_orginal_url#} 【新增】 新增一个缓存关键值类

---
+++ src/main/java/com/movierecomsys/demo/redis/RedisKeys.java
@@ -0,0 +1,14 @@
+package com.movierecomsys.demo.redis;
+
+/**
+ * @author YRC
+ * @date 2021/4/28 11:00
+ **/
+public class RedisKeys {
+
+    // 高分电影
+    public static final String HIGH_MOVIE = "high_movie";
+    public static final String HOT_MOVIE = "hot_movie";
+
+
+}
