1fb3bfdc42e3d2029a427ca0a02c7a27b48d81af
yrc <419107280@qq.com>
2021-05-06 02:11:11 +0800

    --story=1007765 --user=SC 后端开发 {#_orginal_url#} 【新增】 新增一个Redis类

---
+++ src/main/java/com/movierecomsys/demo/redis/RedisApi.java
@@ -0,0 +1,73 @@
+package com.movierecomsys.demo.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author YRC
+ * @date 2021/4/21 16:21
+ **/
+
+@Component
+public class RedisApi {
+
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+
+    /**
+     * 读取缓存
+     *
+     * @param key
+     * @return
+     */
+    public String get(final String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    /**
+     * 将写入缓存
+     */
+    public boolean set(final String key, String value) {
+        boolean result = false;
+        try {
+            redisTemplate.opsForValue().set(key, value, 2 , TimeUnit.HOURS);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 更新缓存
+     */
+    public boolean getAndSet(final String key, String value) {
+        boolean result = false;
+        try {
+            redisTemplate.opsForValue().getAndSet(key, value);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 删除缓存
+     */
+    public boolean delete(final String key) {
+        boolean result = false;
+        try {
+            redisTemplate.delete(key);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
